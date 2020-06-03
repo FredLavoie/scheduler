6d8002ed5486aef4d4a35e83aeaec0f0584aecb6
FredLavoie <flavoie3@gmail.com>
2020-06-03 18:58:32 -0400

    Refactor state related code to useApplicationData hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,69 +1,21 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import "./Application.scss";
 import DayList from "./DayList.js";
 import Appointment from "./Appointment/index.js";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors.js";
+import useApplicationData from "../hooks/useApplicationData"
 
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview
+  } = useApplicationData();
 
-
-  const setDay = day => setState({...state, day});
-  const setDays = days => setState(prev => ({ ...prev, days}));
-  const setAppointments = appointments => setState(prev => ({ ...prev, appointments}));
-  const setInterviewers = interviewers => setState(prev => ({ ...prev, interviewers}));
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios
-      .put(`/api/appointments/${id}`, {interview})
-      .then(() => {setState({...state, appointments})});
-  }
-
-  function deleteInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios
-      .delete(`/api/appointments/${id}`, {id})
-      .then(() => {setState({...state, appointments})});
-  }
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ])
-    .then((all) => {
-      setDays(all[0].data);
-      setAppointments(all[1].data);
-      setInterviewers(all[2].data);
-    })
-  }, []);
 
   const interviewers = getInterviewersForDay(state, state.day);
   const dayAppointments = getAppointmentsForDay(state, state.day).map((appointment) => {
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({ ...prev, days}));
+  const setAppointments = appointments => setState(prev => ({ ...prev, appointments}));
+  const setInterviewers = interviewers => setState(prev => ({ ...prev, interviewers}));
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ])
+    .then((all) => {
+      setDays(all[0].data);
+      setAppointments(all[1].data);
+      setInterviewers(all[2].data);
+    })
+  }, []);
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios
+      .put(`/api/appointments/${id}`, {interview})
+      .then(() => {setState({...state, appointments})});
+  }
+
+  function deleteInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios
+      .delete(`/api/appointments/${id}`, {id})
+      .then(() => {setState({...state, appointments})});
+  }
+
+  return {
+    state: state,
+    setDay: setDay,
+    bookInterview: bookInterview,
+    deleteInterview: deleteInterview
+  };
+}
